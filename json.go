f8ee9bb4288fdafcc741c286dd6ff132fa0cdff8
Hao Nguyen <hao@haonguyen.tech>
2023-07-17 21:32:37 +0700

    feat: add new json helepr

---
+++ json.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(payload)
+	if err != nil {
+		log.Printf("Error: failed to encode the payload %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func WriteErrorJson(w http.ResponseWriter, statusCode int, msg string) {
+	type ApiError struct {
+		Error string `json:"error"`
+	}
+	WriteJSON(w, statusCode, ApiError{Error: msg})
+}
